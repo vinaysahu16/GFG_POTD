d8a66fbfa94989c6a4b2fee9d1bb59431e1bfc66
Vinay Sahu <74305356+vinaysahu16@users.noreply.github.com>
2023-03-30 23:52:15 +0530

    Create 30March_gfg_potd.cpp

---
+++ 30March_gfg_potd.cpp
@@ -0,0 +1,23 @@
+public:
+    int minimumInteger(int N, vector<int> &A) {
+        // code here
+        long long s=0;
+        for(int i=0;i<N; i++)
+        {
+            s+=A[i];
+        }
+        
+        
+        long long ans=INT_MAX;
+        for(int i=0;i<N; i++)
+        {
+            long long x=A[i];
+            if(s<=N*x)
+            {
+                ans=min(ans,x);
+            }
+        }
+        
+        return ans;
+    }
+};
